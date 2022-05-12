c9e86955ac790892b2d90b027eb0eda3119944c1
marcio1002 <marcioalemao1999@yahoo.com>
2022-05-12 01:47:00 -0300

    Correção no layout e adição de interação no perfil

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,25 +1,26 @@
-import 'dotenv/config'
-import React from 'react'
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import "dotenv/config";
+import React from "react";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
 
 export default class MyDocument extends Document {
-
   render() {
     return (
       <Html lang="pt-BR">
         <Head>
+          <link rel="shortcut icon" href="/civil.png" type="image/png" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link rel="shortcut icon" href="/civil.png" type="image/png" />
+          <link
+            href="https://fonts.googleapis.com/css2? family=Dongle:wght@300;400;700&family=Fira+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
           <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Dongle:wght@300;400;700&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <body>
           <Main />
